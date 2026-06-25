5e3ffb0de8d2580ec3cf07b4ff1b23d67af01407
Shivansh01-byte <kumarshivansh4424@gmail.com>
2026-06-25 06:26:47 +0000

    Add 01_variable.js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 5039;
+let accountEmail = "shivansh123@...com";
+var password = "password";
+let accountCity = "Lucknow";
+
+console.log(accountId);
+
+// Update values
+accountEmail = "hgfd@13.in";
+password = "hello";
+accountCity = "Udaipur";
+
+console.table([accountId, accountEmail, password, accountCity]);
