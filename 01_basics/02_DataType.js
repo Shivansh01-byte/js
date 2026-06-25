2a83412dd233d58e86348c5fb4af2306bb314272
Shivansh01-byte <kumarshivansh4424@gmail.com>
2026-06-25 07:18:08 +0000

    commit is done

---
+++ 01_basics/02_DataType.js
@@ -0,0 +1,20 @@
+"use strict" //treat all js code as a newer version//
+// alert (3 + 3) //we are using nodejs,not browser
+console.log( 3 + 3 ,"Shivansh") //code usability should be high//
+// console.log("Shivansh")
+//read documentation through tc39 or mdn
+let name = "Shivansh"
+let age = 18
+let isLoggedIn = false
+let state = null
+//number =. to the power 53
+// bigint
+// string => ""
+//boolean => true/false
+//null = standalone value
+// undefined =>
+// symbols => unique
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof isLoggedIn)
+console.log(typeof state)
