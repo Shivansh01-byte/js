a9d4d7ce85e23fe50a48fe3d136ff94232217a34
Shivansh01-byte <kumarshivansh4424@gmail.com>
2026-06-30 05:23:53 +0000

    num and math

---
+++ 01_basics/06_num_and_mahs.js
@@ -0,0 +1,26 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber = 123.86
+// console.log(otherNumber.toPrecision(3));
+
+const hundred = 100000
+// console.log(hundred.toLocaleString('en-IN'));
+
+// -------------------Maths----------------------
+console.log(Math);
+//  console.log(Math.abs(-3214));
+//  console.log(Math.round(21.4));
+//  console.log(Math.ceil(5.1));
+//  console.log(Math.floor(4.9));
+//  console.log(Math.min(4,5,6,3,2,4,5,7,9));
+//  console.log(Math.max(5,2,4,5,9,8,7,0));
+ console.log(Math.random());
+ console.log((Math.random() * 10) + 1);
+ console.log(Math.floor(Math.random() * (Math.max - Math.min + 1) + Math.min))
