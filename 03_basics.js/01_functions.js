c8ff7cb796c564246ea26c77744adaaa888cf8e3
Shivansh01-byte <kumarshivansh4424@gmail.com>
2026-07-01 09:11:37 +0000

    functions

---
+++ 03_basics.js/01_functions.js
@@ -0,0 +1,36 @@
+function sayMyName(){
+// console.log("s");
+// console.log("h");
+// console.log("i");
+// console.log("v");
+// console.log("a");
+// console.log("n")
+// console.log("s");
+// console.log("h");
+     //=> executes the function
+
+// function addTwoNumbers(number1, number2){
+// console.log(number1 + number2)
+// }
+// function addTwoNumbers(number1, number2){
+//     let result = number1 + number2 
+//     // console.log("Shivansh")
+//     return result
+    //  console.log("Shivansh").   ===>does not executes after return statement
+// }
+// const result = addTwoNumbers(3,5)
+// console.log("Result:",result)
+// addTwoNumbers(3,7)
+// addTwoNumbers(3,"6")
+// addTwoNumbers(3,"a")
+// addTwoNumbers(3,null)
+ 
+// function loginUserMessage(username){
+//     if (!username){
+//         console.log("Please enter your username")
+//         return
+//     }
+//     return '${username} just logged in'
+// }
+//     console.log(loginUserMessage("Shivansh"))
+//     console.log(loginUserMessage()) 
