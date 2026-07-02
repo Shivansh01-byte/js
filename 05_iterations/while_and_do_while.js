6d3b28d46f3cd10fb5f3d0b677880acce0ef89db
Shivansh01-byte <kumarshivansh4424@gmail.com>
2026-07-02 10:31:14 +0000

    while and do while loop

---
+++ 05_iterations/while_and_do_while.js
@@ -0,0 +1,21 @@
+// while and do while loops are used to execute a block of code repeatedly as long as a specified condition is true.
+
+// The while loop checks the condition before executing the block of code, while the do-while loop executes the block of code at least once before checking the condition.
+
+// Here is an example of a while loop:
+
+let i = 0;
+while(i < 5){
+    console.log(i);
+    i++;
+}
+
+// And here is an example of a do-while loop:
+
+let j = 0;
+do{
+    console.log(j);
+    j++;
+}while(j < 5);
+
+// In both examples, the loop will print the numbers from 0 to 4.
