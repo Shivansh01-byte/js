8f272d2f0ecadf147cc73dee9ce1a8553f616d91
Shivansh01-byte <kumarshivansh4424@gmail.com>
2026-07-02 05:04:16 +0000

    scopes

---
+++ 03_basics.js/02_scopes.js
@@ -0,0 +1,41 @@
+// var c = 300
+// let a = 300
+// if(true) {
+//     let a = 10
+//     const b = 20
+//     console.log("Inner:",a)
+// }
+// console.log(a)
+// // console.log(b)
+// console.log(c)
+
+function one(){
+    const username = "shivansh"
+function two(){
+    const website = "youtube"
+    // console.log(username);
+}
+// console.log(website);
+two()
+}
+one()
+if(true){
+    const username = "shivansh"
+    if (username === "shivansh"){
+        const website = "youtube"
+    // console.log(username + website);
+}
+// console.log(website);
+}
+// console.log(username);
+
+function addone(num) {
+    return num + 1
+}
+console.log(addone(5))
+
+
+const addTwo = function (num){
+return num + 2
+}
+console.log(addTwo(5))
