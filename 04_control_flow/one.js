018ab6f2731d8d263b867011fd8dd7d203f8eccf
Shivansh01-byte <kumarshivansh4424@gmail.com>
2026-07-02 09:56:41 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,10 @@
+// if 
+const balance = 1200;
+
+if (balance === 1000 ) {
+    console.log("balance is 1000");
+} else if (balance > 750) {
+    console.log("balance is greater than 750");
+} else if (balance > 500) {
+    console.log("balance is greater than 500");
+}
